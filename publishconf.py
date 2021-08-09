90570123eab1138b516442432a17002149476d76
Daniel Duckworth <duckworthd@gmail.com>
2021-08-09 12:48:31 +0300

    http:// --> https://

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://stronglyconvex.com'
+SITEURL = 'https://stronglyconvex.com'
 RELATIVE_URLS = False
 
 # Following items are often useful when publishing
